92c15042ae0c904f6985a2d95892e8adcc51bc9b
KianaJaberi <152029042+KianaJaberi@users.noreply.github.com>
2024-02-17 13:54:17 +0330

    Enemy spaceship

---
+++ enemy spaceship.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std ;
+
+int main (){
+	
+	//input : size
+	//output : enemy spaceship type , enemy spaceship heal , enemy spaceship location
+	
+	int size ;
+	
+	char type ;
+	int heal ;
+	int loc ;
+		
+	srand ( time (0) ) ;
+	
+	int random = rand () % 4 ;
+		
+	switch ( random ){
+		case 0 : type = 'd' , heal = 1 ; //type : dart
+			break ;
+		case 1 : type = 's' , heal = 2 ; //type : Striker
+			break ;
+		case 2 : type = 'w' , heal = 4 ; //type : Wraith
+			break ;
+		case 3 : type = 'b' , heal = 6 ; //type : Banshee
+			break ;
+	}
+	
+	while ( true ){
+	
+		switch ( type ){
+			case 'd' : loc = rand () % size ;
+				break ;
+			case 's' : loc = rand () % size + 1 ;
+				break ;
+			case 'w' : loc = rand () % size + 2 ;
+				break ;
+			case 'b' : loc = rand () % size + 3 ;
+				break ;
+		}
+		
+		if ( loc > size )
+			continue ;
+		
+		break ;
+	}
+	
+	return 0 ;
+}
